50d27eea7c0599ed7617c2cf2c5aa717b2c92db6
HeadPain <kiri.verushkin@gmail.com>
2018-02-26 21:13:22 +0300

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Эльдар Рязанов'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Иполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
